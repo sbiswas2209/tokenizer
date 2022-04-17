6b6b07de0e6e77c2ac8d2726bae73472ed5f794c
Sagnik Biswas <me@sagnikbiswas.tech>
2022-04-17 23:10:34 +0530

    :sparkles: Add Base Features

---
+++ pages/api/tokenize.ts
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import jsTokens from 'js-tokens'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log(req.body)
+  res.status(200).json({ tokens: Array.from(jsTokens(req.body.input), (token) => token.value).filter((e) => e !== " ").join("|") })
+}
